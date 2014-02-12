21b0aa88871f1a5fc91f334ff3f8657ddcbf0299
Bartosz Kosarzycki <bartek@brodline.com>
2014-02-12 22:08:27 +0100

    Simple timer + TextView&Button

--- src/main/scala/org/scaloid/hello/AnnotationTestActivity.java
+++ src/main/scala/org/scaloid/hello/AnnotationTestActivity.java
@@ -1,20 +1,54 @@
 package org.scaloid.hello;
 
 import android.app.Activity;
-import com.googlecode.androidannotations.annotations.AfterInject;
-import com.googlecode.androidannotations.annotations.EActivity;
+import android.widget.TextView;
+import com.googlecode.androidannotations.annotations.*;
 
-/**
- * Created by kosiara on 12.02.14.
- */
 @EActivity(R.layout.simple_textbox_layout)
-//@EActivity(resName = "dashboard")
 public class AnnotationTestActivity extends Activity {
 
-    @AfterInject
-    protected void afterInject() {
+    @ViewById(R.id.textChangedEverySecond)
+    public TextView textView;
 
+    private long counter;
+
+    @AfterInject
+    public void afterInject() {}
+
+    @AfterViews
+    public void afterViews() {
+        textView.setText("Let's start counting....");
+        changeTextEverySecond();
     }
 
+    @Click(R.id.buttonChangeLabel)
+    public void click() {
+        handleTextChangeButtonPressed();
+    }
+
+    @Background
+    public void changeTextEverySecond() {
+        for (;;) {
+            counter++;
+            handleTextChange();
+            sleep();
+        }
+    }
+
+    @UiThread
+    public void handleTextChange() {
+        if (textView != null)
+            textView.setText(counter + " seconds passed");
+    }
+
+    @UiThread
+    public void handleTextChangeButtonPressed() {
+        if (textView != null)
+            textView.setText("Button pressed");
+    }
+
+    private void sleep() {
+        try  { Thread.sleep(1000); } catch (Exception exc) {}
+    }
 
 }
